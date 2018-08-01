5cf61cfbb54090d7502d9d3c144f60b008daefa2
Iukekini <justindaw@gmail.com>
2018-07-31 23:18:40 -0600

    Removed unused functions and added comments

--- main.go
+++ main.go
@@ -13,10 +13,12 @@ import (
 	"gopkg.in/inconshreveable/log15.v2"
 )
 
+//Setup Logging
 var log = log15.New()
 
 func main() {
 
+	//Setup the Flags from the commandline. 
 	ipaddress := flag.String("ip", "", "IP Address of Polycom Phone")
 	username := flag.String("username", "Polycom", "API Username")
 	password := flag.String("password", "", "Password for API")
@@ -24,35 +26,31 @@ func main() {
 
 	flag.Parse()
 
+	//Check to see if we got an IP Address of a phone. 
 	if *ipaddress == "" {
 		log.Error("No IPAddress Specified for Phone")
 		os.Exit(1)
 	}
+
+	//Make sure that a password was also passed in. 
 	if *password == "" {
 		log.Error("No Password Specified")
 		os.Exit(1)
 	}
 
+	
 	switch *action {
 		case "networkinfo":
+			//Get the Networking Information from Phone. 
 			networkResponse := networkInfo(*ipaddress, *username, *password)
+			//Print Response out. Nicely formatted. 
 			printObject(networkResponse)
 			break
 	}
 
 }
 
-func printObject(obj interface {}){
-	b, err := json.MarshalIndent(obj, "", "  ")
-	if err != nil {
-		log.Error(err.Error())
-		os.Exit(1);
-	}
-	os.Stdout.Write(b)
-
-}
-
-//NetworkInfo provides details about the phones's network information
+//NetworkInfo provides details about the phones's network information. IPAddress/ Vlan / DHCP / ETC
 func networkInfo(phoneIP string, username string, password string) NetworkInfoResponse {
 	var path = "https://" + phoneIP + "/api/v1/mgmt/network/info"
 	log.Info("Attempting to connect to phone via:", path, nil)
@@ -72,31 +70,32 @@ func networkInfo(phoneIP string, username string, password string) NetworkInfoRe
 }
 
 
-func basicAuth(username string, password string) string {
-	auth := username + ":" + password
-	return base64.StdEncoding.EncodeToString([]byte(auth))
-}
-
-func redirectPolicyFunc(req *http.Request, via []*http.Request) error {
-	return nil
-}
-
+// callPhoneAPI calls a given VVX Phones with the creds and path requested. You can pass a body and select what method(GET/POST) will be used to make the call. 
+//It returns the response if valid. If not it exits
 func callPhoneAPI(url string, username string, password string, body string, method string) *http.Response {
 
+	//Skip validation of cert. Per Polycom Insturctions. 
 	http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
 
 	client := new(http.Client)
 
 	resp, err := http.NewRequest(method, url, bytes.NewBufferString(body))
 
+	//Add Headers
 	resp.Header.Add("Authorization", "Basic "+basicAuth(username, password))
 	resp.Header.Add("Accept", "application/json")
 
+	//Make call to phone. 
 	response, err := client.Do(resp)
 
+
+	//Check for an error making the call. Timeouts are caught here. 
 	if err != nil {
 		log.Error("Error getting response: ", err.Error())
+		os.Exit(1)
 	}
+
+	//Checks to make sure we got a successful call. If not display the message received from the phone and exit. 
 	if response.StatusCode != 200 {
 		log.Error(strconv.Itoa(response.StatusCode) + ":" + response.Status)
 		os.Exit(1)
@@ -106,4 +105,22 @@ func callPhoneAPI(url string, username string, password string, body string, met
 
 	return response
 
+}
+
+
+/// printOject takes a struct and geneates a nicely formated json file. This is used to display the informatino to the user. 
+func printObject(obj interface {}){
+	b, err := json.MarshalIndent(obj, "", "  ")
+	if err != nil {
+		log.Error(err.Error())
+		os.Exit(1);
+	}
+	os.Stdout.Write(b)
+
+}
+
+/// basicAuth generates the value for the basic auth header. 
+func basicAuth(username string, password string) string {
+	auth := username + ":" + password
+	return base64.StdEncoding.EncodeToString([]byte(auth))
 }
